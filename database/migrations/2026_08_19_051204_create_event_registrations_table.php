d7cd30413119dd85266cd448d058bedc8e54b53c
mannan608 <mannanswe608@gmail.com>
2026-08-25 15:53:35 +0600

    update

--- database/migrations/2026_08_19_051204_create_event_registrations_table.php
+++ database/migrations/2026_08_19_051204_create_event_registrations_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('event_registrations', function (Blueprint $table) {
             $table->uuid('id')->primary();
 
-            $table->uuid('event_id');
+             $table->foreignId('event_id');
 
             $table->string('full_name', 150);
             $table->string('email', 191);
